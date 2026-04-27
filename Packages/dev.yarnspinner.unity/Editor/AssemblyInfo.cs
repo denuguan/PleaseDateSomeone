76e91e4c2958d0bf01c2ac14e1631f0f4fd23079
denuguan <christinesatorre15@gmail.com>
2026-04-27 13:34:39 -0500

    first commit
    importing

---
+++ Packages/dev.yarnspinner.unity/Editor/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿/*
+Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("3.2.2.0")]
+[assembly: AssemblyFileVersion("3.2.2.0")]
+[assembly: AssemblyInformationalVersion("3.2.2-1+Branch.main.Sha.b4544822ea88812eb3ab7102fc54ad84e367cd96")]
+
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Tests.Editor")]
